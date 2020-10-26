6ae4825cb179f3db93e802ec4052b3c68edcd2cc
qq1403332591 <1403332591@qq.com>
2020-10-26 22:20:02 +0800

    显示等待查找元素

---
+++ testing/test_xianshiwait.py
@@ -0,0 +1,26 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class Test_webdriverwait():
+    def setup(self):
+        options = Options()  # 调试模式开启时必须要配置一个options
+        options.debugger_address = "127.0.0.1:9222"  # debug的地址
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.get("https://ceshiren.com/")
+        self.driver.implicitly_wait(3)
+
+    def test01_case(self):
+        self.driver.find_element(By.XPATH, '//*[@id="ember43"]').click()
+
+        def wait(x):
+            return len(self.driver.find_elements(By.CLASS_NAME, 'category-name')) >= 1
+
+        WebDriverWait(self.driver, 10).until(wait)
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, '//*[@id="ember194"]/a/div/span').click()
